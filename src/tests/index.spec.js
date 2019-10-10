42d288c64b64641d537a373dfea21e2eb0629971
Денис <denis.darwin12@gmail.com>
2019-10-10 21:44:01 +0300

    Исправили весь код по eslint

--- src/tests/index.spec.js
+++ src/tests/index.spec.js
@@ -2,14 +2,14 @@ import myFunction from '../index'
 
 // Совокупность тесткейсов
 describe('myFunction', () => {
-    it ('get first', () => {
+    it('get first', () => {
         expect(myFunction([1, 2, 3])).toBe(1)
         expect(myFunction([3, 2, 1])).toBe(3)
     })
-    it ('get first (wrong)', () => {
+    it('get first (wrong)', () => {
         expect(myFunction([1, 2, 3])).toBe(1)
     })
-    it ('default', () => {
+    it('default', () => {
         expect(myFunction()).toBe(undefined)
     })
 })
