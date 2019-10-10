1ce226fc63367fc4277785870cdb10f198a4676b
Денис <denis.darwin12@gmail.com>
2019-10-10 21:40:15 +0300

    Подключили eslint

--- src/tests/index.spec.js
+++ src/tests/index.spec.js
@@ -1,13 +1,15 @@
 import myFunction from '../index'
 
-//Совокупность тесткейсов
+// Совокупность тесткейсов
 describe('myFunction', () => {
-
-    it ('get first' , () => {
-        expect(myFunction ([1,2,3])).toBe(1)
-        expect(myFunction ([3,2,1])).toBe(3)
+    it ('get first', () => {
+        expect(myFunction([1, 2, 3])).toBe(1)
+        expect(myFunction([3, 2, 1])).toBe(3)
     })
     it ('get first (wrong)', () => {
-        expect(myFunction ([1,2,3])).toBe(1)
+        expect(myFunction([1, 2, 3])).toBe(1)
+    })
+    it ('default', () => {
+        expect(myFunction()).toBe(undefined)
     })
 })
